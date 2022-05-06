325140dd5c80ab354f0bb974fcd3af6ce8198ca3
karima <timmermann@gmx.at>
2022-05-06 16:18:34 +0200

    Refactor and fix bug in function

--- src/control/placeShipsComputer.js
+++ src/control/placeShipsComputer.js
@@ -1,130 +1,115 @@
-
 const placeShipComputer = (() => {
-    let shipLengths = [5,4,3,3,2]
-    let allShipCoord = []
-    let myShipCoord = []
-    let testShipOverlap = []
-    let boardA
+    let shipLengths = [5,4,3,3,2];
+    let allShipCoord = [];
 
     function doPlacement( board ){
-        boardA = board
+        let boardA = board;
         do {
-            testShipOverlap = []
-            myShipCoord = []
-            Math.random()
-            if ( Math.floor(Math.random()*2) == 0 ) {  // vertical ship
-                let xaxis = Math.floor(Math.random()*99)
-                let above = Math.floor(Math.random()*2)
-                console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-                console.log(xaxis)
-                console.log(above)
-                if (above == 0) {
-                    if (xaxis + (10*shipLengths[0]) <100) {
-                        _placeShipAbove(xaxis)
-                    } else {
-                        _placeShipBelow(xaxis)
-                    }
-                } else {
-                    if (xaxis - (10*shipLengths[0]) >-1) {
-                        _placeShipBelow(xaxis)
-                    } else {
-                        _placeShipAbove(xaxis)
-                    }
-                }
-            } else {  // horizontal ship
-                let random =  Math.floor(Math.random()*99)
-                let yaxis = Math.floor(random/10)
-                let ybasis = random%10
-                let candidates =  []
-                for (let i=0; i<=9; i++) {
-                    candidates.push(ybasis*10+i)
-                }
-                
-                let left = Math.floor(Math.random()*2)
-                console.log("yyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
-                console.log(yaxis)
-                console.log(left)
-                if (left == 0) {
-                    if ( candidates.includes(yaxis + shipLengths[0]) ) {
-                        _placeShipRight(yaxis)
-                    } else {
-                        _placeShipLeft(yaxis)
-                    }
-                } else {
-                    if ( candidates.includes(yaxis - shipLengths[0]) ) {
-                        _placeShipLeft(yaxis)
-                    } else {
-                        _placeShipRight(yaxis)
-                    }
-                }
+            let candidates = [];
+            let placement;      // an array with coordinates of a ship candidate or false (if)
+            let start = Math.floor(Math.random()*99); 
+            
+            // Starting from a random cell, push into candidates ships that fall into the board and do not overlap.
+            placement = _placeUP( start );
+            if (placement) { placement = _testShipOverlap(placement) };
+            if (placement) { candidates.push(placement) };
+
+            placement = _placeDOWN( start );
+            if (placement) { placement = _testShipOverlap(placement) };
+            if (placement) { candidates.push(placement) };
+
+            placement = _placeLEFT( start );
+            if (placement) { placement = _testShipOverlap(placement) };
+            if (placement) { candidates.push(placement) };
+
+            placement = _placeRIGHT( start );
+            if (placement) { placement = _testShipOverlap(placement) };
+            if (placement) { candidates.push(placement) };
+
+            if (candidates.length > 0) {
+                let rd = Math.floor(Math.random()*candidates.length); 
+                allShipCoord.push( ...candidates[rd] );
+                boardA.placeShip( candidates[rd] );
+                shipLengths.splice(0,1);
             } 
-        } while ( shipLengths.length > 0)
-    }
+            console.log(boardA);
+        } while (shipLengths.length>0);
+    };
 
-    function _placeShipAbove(basis) {
-        for (let i=0; i<shipLengths[0]; i++) { 
-            myShipCoord.push(basis + 10*i)
+    function _testShipOverlap(placement) {
+        const filteredArray = placement.filter(value => allShipCoord.includes(value));
+        if ( filteredArray.length>0 ) {
+            placement = false;
         }
-        for (let i=0; i<shipLengths[0]; i++) { 
-            if (!allShipCoord.includes(basis + 10*i)) {
-                testShipOverlap.push(1)
-            }
-        }
-        if (testShipOverlap.length == shipLengths[0] ) {
-            boardA.placeShip( myShipCoord )
-            allShipCoord.push( ...myShipCoord )
-            shipLengths.shift()
-        }
-    }
+        return placement;    
+    };
 
-    function _placeShipBelow(basis) {
+    function _placeUP( basis ) {
+        let test=[];
         for (let i=0; i<shipLengths[0]; i++) { 
-            myShipCoord.push(basis - 10*i)
-        }
-        for (let i=0; i<shipLengths[0]; i++) { 
-            if (!allShipCoord.includes(basis - 10*i)) {
-                testShipOverlap.push(1)
+            let candidate = basis - 10*i;
+            if (candidate >= 0) {
+               test.push(candidate);
             }
+        } 
+        if (test.length == shipLengths[0]) {
+           return test;
+        } else {
+           return false;
         }
-        if (testShipOverlap.length == shipLengths[0] ) {
-            boardA.placeShip( myShipCoord )
-            allShipCoord.push( ...myShipCoord )
-            shipLengths.shift()
-        }
-    }
+    };
 
-    function _placeShipRight(basis) {
+    function _placeDOWN( basis ) {
+        let test=[];
         for (let i=0; i<shipLengths[0]; i++) { 
-            myShipCoord.push(basis + i)
-        }
-        for (let i=0; i<shipLengths[0]; i++) { 
-            if (!allShipCoord.includes(basis + i)) {
-                testShipOverlap.push(1)
+            let candidate = basis + 10*i;
+            if (candidate <= 99) {
+               test.push(candidate);
             }
+        } 
+        if (test.length == shipLengths[0]) {
+           return test;
+        } else {
+           return false;
         }
-        if (testShipOverlap.length == shipLengths[0] ) {
-            boardA.placeShip( myShipCoord )
-            allShipCoord.push( ...myShipCoord )
-            shipLengths.shift()
-        }
-    }
+    };
 
-    function _placeShipLeft(basis) {
+    function _placeLEFT( basis ) {
+        let test=[];
         for (let i=0; i<shipLengths[0]; i++) { 
-            myShipCoord.push(basis - i)
-        }
-        for (let i=0; i<shipLengths[0]; i++) { 
-            if (!allShipCoord.includes(basis - i)) {
-                testShipOverlap.push(1)
+            let candidate = basis-(1*i);
+            if ( Math.floor( (candidate*10)/100) == Math.floor( (basis*10)/100)) {
+                test.push(candidate);
+            } else if ( basis<10 && candidate>=0) {
+                test.push(candidate);
             }
+        } 
+        if (test.length == shipLengths[0]) {
+           return test;
+        } else {
+           return false;
         }
-        if (testShipOverlap.length == shipLengths[0] ) {
-            boardA.placeShip( myShipCoord )
-            allShipCoord.push( ...myShipCoord )
-            shipLengths.shift()
+    };
+
+    function _placeRIGHT( basis ) {
+        let test=[];
+        for (let i=0; i<shipLengths[0]; i++) { 
+            let candidate = basis+(1*i);
+            if ( Math.floor( (candidate*10)/100) == Math.floor( (basis*10)/100)) {
+                test.push(candidate);
+            } else if ( basis<10 && candidate<10) {
+                test.push(candidate);
+            }
+        } 
+        if (test.length == shipLengths[0]) {
+           return test;
+        } else {
+           return false;
         }
-    }
+    };
+
     return { doPlacement }
+
 })()
 
-export default placeShipComputer
+export default placeShipComputer;
